be9795b79aa5fbd5b2f84c1b595e497abeb8d583
Suyash Nayak <suyashnayak11@gmail.com>
2026-02-10 13:52:04 +0530

    Update 05_category_roi_analysis.sql

--- SQL Queries/05_category_roi_analysis.sql
+++ SQL Queries/05_category_roi_analysis.sql
@@ -1,1 +1,15 @@
 
+
+-- Category/Platform Preformance
+
+select 
+category, 
+round(sum(mark_spent), 2) as Total_Spend,
+round(sum(revenue), 2) as Total_Revenue,
+round((sum(revenue)-sum(mark_spent))*100/sum(mark_spent), 2) as ROI_Percentage,
+rank() over( order by (sum(revenue)-sum(mark_spent))*1.0/sum(mark_spent) desc) as ROI_Rank
+from marketing
+group by category;
+
+
+-- Category-level analysis reveals that Influencer marketing is the most profitable channel, delivering a 154% ROI. Media and Search campaigns show limited but positive returns, while Social campaigns generate consistent losses despite high spend. Reallocating budget toward high-performing categories can significantly improve overall marketing profitability.
