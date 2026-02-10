ae56f93c9a0073a276cabc3b49bffc5996610e18
Suyash Nayak <suyashnayak11@gmail.com>
2026-02-10 14:01:32 +0530

    Update 06_time_based_trends.sql

--- SQL Queries/06_time_based_trends.sql
+++ SQL Queries/06_time_based_trends.sql
@@ -1,1 +1,11 @@
 
+-- Date wise Preformance
+
+select 
+c_date, 
+round(sum(mark_spent), 2) as Total_Spend,
+round(sum(revenue), 2) as Total_Revenue,
+round((sum(revenue)-sum(mark_spent))*100/sum(mark_spent), 2) as ROI_Percentage,
+rank() over( order by (sum(revenue)-sum(mark_spent))*1.0/sum(mark_spent) desc) as ROI_Rank
+from marketing
+group by c_date;
